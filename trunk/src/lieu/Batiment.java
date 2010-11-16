0630787266effc8de27a81d5de03bd8026ffa123
nicolas.poirier91@orange.fr <nicolas.poirier91@orange.fr@43486c22-1e13-4fa2-52d3-2fa976c108dc>
2010-11-16 22:27:30 +0000

    Ajout de javadoc

--- trunk/src/lieu/Batiment.java
+++ trunk/src/lieu/Batiment.java
@@ -1,7 +1,15 @@
 package lieu;
 
+/**
+ * Un lieu accessible uniquement à pied.
+ * @author Nicolas
+ */
 public class Batiment extends Lieu
 {
+	/**
+	 * Construit un batiment avec un nom.
+	 * @param nom le nom du bâtiment.
+	 */
 	public Batiment(String nom)
 	{
 		super(nom);
--- trunk/src/lieu/Lieu.java
+++ trunk/src/lieu/Lieu.java
@@ -2,22 +2,49 @@ package lieu;
 
 import java.util.ArrayList;
 
+/**
+ * Un lieu abstrait représenté par un nom et une liste de voisins à pied.
+ * @author Nicolas
+ */
 public abstract class Lieu
 {
+	/**
+	 * Le nom de la ligne.
+	 * @see Lieu#public Lieu(String nom)
+	 */
 	protected String nom;
+	
+	/**
+	 * La liste des voisins à pied.
+	 * On peut ajouter un nouveau voisin à pied.
+	 * @see Lieu#ajouterVoisinAPied(Lieu voisin)
+	 */
 	private ArrayList<Lieu> voisinsAPied;
 	
+	/**
+	 * Construit un lieu avec un nom.
+	 * @param nom le nom du lieu
+	 */
 	public Lieu(String nom)
 	{
 		voisinsAPied = new ArrayList<Lieu>();
 		this.nom = nom;
 	}
 	
+	/**
+	 * Ajoute au lieu un nouveau voisin à pied.
+	 * @param voisin le nouveau lieu voisin.
+	 */
 	public void ajouterVoisinAPied(Lieu voisin)
 	{
 		voisinsAPied.add(voisin);
 	}
 	
+	/**
+	 * Teste si un lieu est accessible à pied depuis le lieu courant.
+	 * @param lieu le lieu à tester.
+	 * @return true si le lieu est accessible à pied depuis le lieu courant, false sinon.
+	 */
 	public boolean estAccessibleAPied(Lieu lieu)
 	{
 		return voisinsAPied.contains(lieu);
