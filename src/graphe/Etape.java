51a3b873d2850af9ede2b800def5cfde84df67dd
remi.lacroix <remi.lacroix@43486c22-1e13-4fa2-52d3-2fa976c108dc>
2010-12-12 13:27:01 +0000

    Fichiers manquants

---
+++ src/graphe/Etape.java
@@ -0,0 +1,103 @@
+package graphe;
+
+import ligne.Ligne;
+import moyenstransport.MoyenTransport;
+
+
+public class Etape
+{
+	private Lieu lieuDepart;
+	private Lieu lieuArrivee;
+	private MoyenTransport moyenTransport;
+	private Ligne ligne;
+	private double cout;
+	private int duree;
+	
+	public Etape(Lieu lieuDepart, Lieu lieuArrivee, MoyenTransport moyenTransport, Ligne ligne, double cout, int duree)
+	{
+		this.lieuDepart = lieuDepart;
+		this.lieuArrivee = lieuArrivee;
+		this.cout = cout;
+		this.duree = duree;
+		this.ligne = ligne;
+		
+		if (cout >= 0 && duree >= 0)
+			lieuDepart.ajouterEtapeVersVoisin(this);
+		if (ligne != null)
+			ligne.ajouterTronçon(this);
+	}
+	
+	public Etape(Lieu lieuDepart, Lieu lieuArrivee, MoyenTransport moyenTransport, int duree, boolean bidirectionnel)
+	{
+		this(lieuDepart, lieuArrivee, moyenTransport, null, 0, duree);
+		if (bidirectionnel)
+			new Etape(lieuArrivee, lieuDepart, moyenTransport, duree);
+	}
+	
+	public Etape(Lieu lieuDepart, Lieu lieuArrivee, MoyenTransport moyenTransport, int duree)
+	{
+		this(lieuDepart, lieuArrivee, moyenTransport, null, 0, duree);
+	}
+	
+	public Etape(Lieu lieuDepart, Lieu lieuArrivee, MoyenTransport moyenTransport, Ligne ligne)
+	{
+		this(lieuDepart, lieuArrivee, moyenTransport, ligne, -1, -1);
+	}
+	
+	public Etape(Lieu lieuDepart, Lieu lieuArrivee, MoyenTransport moyenTransport)
+	{
+		this(lieuDepart, lieuArrivee, moyenTransport, null, -1, -1);
+	}
+	
+	public Lieu getLieuDepart()
+	{
+		return lieuDepart;
+	}
+	
+	public Lieu getLieuArrivee()
+	{
+		return lieuArrivee;
+	}
+	
+	public MoyenTransport getMoyenTransport()
+	{
+		return moyenTransport;
+	}
+	
+	public Ligne getLigne()
+	{
+		return ligne;
+	}
+	
+	public double getCout()
+	{
+		return cout;
+	}
+	
+	public int getDuree()
+	{
+		return duree;
+	}
+	
+	public boolean setCout(double cout)
+	{
+		if (this.cout < 0)
+		{
+			this.cout = cout;
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	public boolean setDuree(int duree)
+	{
+		if (this.duree < 0)
+		{
+			this.duree = duree;
+			return true;
+		}
+		else
+			return false;
+	}
+}
---
+++ src/graphe/Lieu.java
@@ -0,0 +1,58 @@
+package graphe;
+
+import java.util.ArrayList;
+
+/**
+ * Un lieu abstrait représenté par un nom et une liste de voisins.
+ * @author Nicolas
+ */
+public class Lieu
+{
+	/**
+	 * Le nom de la ligne.
+	 * @see Lieu#public Lieu(String nom)
+	 */
+	private String nom;
+	
+	/**
+	 * La liste des voisins à pied associée avec la durée pour les atteindre.
+	 * On peut ajouter un nouveau voisin à pied.
+	 * @see Lieu#ajouterVoisinAPied(Lieu voisin)
+	 */
+	private ArrayList<Etape> etapesVersVoisins;
+	
+	/**
+	 * Construit un lieu avec un nom.
+	 * @param nom le nom du lieu
+	 */
+	public Lieu(String nom)
+	{
+		this.nom = nom;
+		etapesVersVoisins = new ArrayList<Etape>();
+	}
+	
+	public String getNom()
+	{
+		return nom;
+	}
+	
+	/**
+	 * Ajoute au lieu un nouveau voisin à pied.
+	 * @param voisin le nouveau lieu voisin.
+	 * @param temps le temps pour atteindre le lieu.
+	 */
+	public void ajouterEtapeVersVoisin(Etape voisin)
+	{
+		etapesVersVoisins.add(voisin);
+	}
+	
+	public boolean propose(Etape etape)
+	{
+		return false;
+	}
+	
+	public String toString()
+	{
+		return nom;
+	}
+}
