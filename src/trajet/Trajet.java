49c740df9b4aef58d7a2d7d8715a8da2fc401b05
nivet1@hotmail.com <nivet1@hotmail.com@43486c22-1e13-4fa2-52d3-2fa976c108dc>
2010-12-15 17:15:09 +0000

    Création de la javadoc de la classe Trajet.java

--- src/trajet/Trajet.java
+++ src/trajet/Trajet.java
@@ -18,6 +18,12 @@ public class Trajet
 		etapes = new LinkedList<Etape>();
 	}
 	
+	/**
+	 * Methode ajouterEtape qui ajoute une étape au trajet
+	 * Retourne une exception si l'étape n'existe pas avec les conditions demandees
+	 * @param etape étape à ajouter
+	 * @throws TrajetException exception de non cohérence entre l'étape demandée et le lieu actuel 
+	 */
 	public void ajouterEtape(Etape etape) throws TrajetException
 	{
 		if (!etape.getLieuDepart().mettreAJourCoutEtDuree(etape))
@@ -28,17 +34,28 @@ public class Trajet
 		
 		etapes.add(etape);
 	}
-	
+	/**
+	 * Methode getLieuDepart
+	 * @return le lieu de depart du trajet
+	 */
 	public Lieu getLieuDepart()
 	{
 		return etapes.getFirst().getLieuDepart();
 	}
 	
+	/**
+	 * Methode getLieuArrivee
+	 * @return le lieu d'arrivee du trajet
+	 */
 	public Lieu getLieuArrivee()
 	{
 		return etapes.getLast().getLieuArrivee();
 	}
 	
+	/**
+	 * Méthode getCout calcule le cout global de toutes les étapes d'un trajet
+	 * @return cout
+	 */
 	public double getCout()
 	{
 		double cout = 0;
@@ -49,6 +66,10 @@ public class Trajet
 		return cout;
 	}
 	
+	/**
+	 * Methode getDuree qui calcule la duree totale de toutes les etapes d'un trajet
+	 * @return duree
+	 */
 	public int getDuree()
 	{
 		int duree = 0;
@@ -59,6 +80,11 @@ public class Trajet
 		return duree;
 	}
 	
+	/**
+	 * Methode passePar qui verifie etant donne un lieu si il appartient à un trajet
+	 * @param lieu
+	 * @return vrai si le lieu appartient au trajet, faux sinon
+	 */
 	public boolean passePar(Lieu lieu)
 	{
 		for (Etape e : etapes)
@@ -70,7 +96,11 @@ public class Trajet
 		return false;
 	}
 	
-
+	/**
+	 * Methode utilise qui verifie si pour un trajet, une étape utilise le moyen de transport voulu
+	 * @param moyenTransport
+	 * @return vrai si le trajet comporte le moyen de transport, faux sinon
+	 */
 	public boolean utilise(MoyenTransport moyenTransport)
 	{
 		for (Etape e : etapes)
@@ -82,6 +112,10 @@ public class Trajet
 		return false;
 	}
 	
+	/**
+	 * Methode toString qui affiche les caracteristiques d'un trajet
+	 * en explicitant les étapes avec leur numéro
+	 */
 	public String toString()
 	{
 		String str = "Trajet de \""+getLieuDepart()+"\" à \""+getLieuArrivee()+"\" :";
