ae18d1bb2c25e3bf2cbcd9f51729c9157d039a1f
remi.lacroix <remi.lacroix@43486c22-1e13-4fa2-52d3-2fa976c108dc>
2010-12-15 22:26:22 +0000

    - Ajout javadoc et commentaires pour les classes du package observateurs et la classe TestSimulateur.

--- trunk/src/observateurs/UsagerAtteintArrivee.java
+++ trunk/src/observateurs/UsagerAtteintArrivee.java
@@ -5,21 +5,36 @@ import java.util.Observer;
 
 import usager.Usager;
 
+/**
+ * Classe implémentant un observateur capable de surveiller les usagers et
+ * détectant l'arrivée d'un usager à destination.
+ */
 public class UsagerAtteintArrivee implements Observer
 {
+	/**
+	 * Crée un nouvel observateur capable de surveiller les usagers et de
+	 * détecter leur arrivée à destination.
+	 */
 	public UsagerAtteintArrivee()
 	{
 		
 	}
 	
+	/**
+	 * Méthode appellée automatiquement lorsqu'un objet surveillé est modifié.
+	 * Affiche un message si un usager a atteint sa destination.
+	 * @param obs l'observable pour lequel un évènement a été détecté par l'observeur.
+	 * @param arg un objet quelconque pouvant servir d'argument à l'observeur.
+	 */
 	public void update(Observable obs, Object arg)
 	{
 		try
-		{
+		{ // On attend un usager et un entier.
 			Usager usager = (Usager)obs;
+			Integer t = (Integer)arg;
 			
 			if (usager.estArrive())
-				System.out.println("Evènement à t = "+arg+" : l'usager "+usager+" vient d'atteindre sa destination ("+usager.getLieuArrivee()+") (temps passé à attendre les transports = "+usager.getTempsAttenduTransport()+").");
+				System.out.println("Evènement à t = "+t+" : l'usager "+usager+" vient d'atteindre sa destination ("+usager.getLieuArrivee()+") (temps passé à attendre les transports = "+usager.getTempsAttenduTransport()+").");
 		}
 		catch (ClassCastException e) { /* on ignore l'événement */ }
 	}
--- trunk/src/observateurs/UsagerQuitteLieu.java
+++ trunk/src/observateurs/UsagerQuitteLieu.java
@@ -7,28 +7,50 @@ import java.util.Observer;
 
 import usager.Usager;
 
+/**
+ * Classe implémentant un observateur capable de surveiller les usagers et
+ * détectant leur départ d'un lieu quelconque ou bien défini.
+ */
 public class UsagerQuitteLieu implements Observer
 {
+	/** Le lieu filtrant les évènements de départ */
 	private Lieu lieu;
 	
+	/**
+	 * Crée un observateur rapportant tous les évènements relatifs au
+	 * départ d'un usager d'un lieu quelconque.
+	 */
 	public UsagerQuitteLieu()
 	{
 		lieu = null;
 	}
 	
+	/**
+	 * Crée un observateur rapportant tous les évènements relatifs au
+	 * départ d'un usager d'un lieu précisé.
+	 * @param lieu le lieu filtrant les évènements.
+	 */
 	public UsagerQuitteLieu(Lieu lieu)
 	{
 		this.lieu = lieu;
 	}
 	
+	/**
+	 * Méthode appellée automatiquement lorsqu'un objet surveillé est modifié.
+	 * Affiche un message si un usager a quitté le lieu défini ou un lieu quelconque 
+	 * selon les cas.
+	 * @param obs l'observable pour lequel un évènement a été détecté par l'observeur.
+	 * @param arg un objet quelconque pouvant servir d'argument à l'observeur.
+	 */
 	public void update(Observable obs, Object arg)
 	{
 		try
-		{
+		{ // On attend un usager et un entier.
 			Usager usager = (Usager)obs;
+			Integer t = (Integer)arg;
 			
 			if ((lieu == null || lieu == usager.getEtapeActuelle().getLieuDepart()) && !usager.estArrive())
-				System.out.println("Evènement à t = "+arg+" : l'usager "+usager+" vient de quitter le lieu "+usager.getEtapeActuelle().getLieuDepart()+".");
+				System.out.println("Evènement à t = "+t+" : l'usager "+usager+" vient de quitter le lieu "+usager.getEtapeActuelle().getLieuDepart()+".");
 		}
 		catch (ClassCastException e) { /* on ignore l'événement */ }
 	}
