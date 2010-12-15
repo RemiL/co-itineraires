85b10ef063e801d4a69eed0df57a23657929d5ec
remi.lacroix <remi.lacroix@43486c22-1e13-4fa2-52d3-2fa976c108dc>
2010-12-15 22:36:09 +0000

    - Ajout javadoc et commentaires pour les classes du package simulateurdeplacement.

--- trunk/src/simulateurdeplacement/Simulable.java
+++ trunk/src/simulateurdeplacement/Simulable.java
@@ -1,6 +1,14 @@
 package simulateurdeplacement;
 
+/**
+ * Interface permettant de regroupant tous les objets dont l'évolution
+ * peut être simulée.
+ */
 public interface Simulable
 {
+	/**
+	 * Permet de simuler le passage d'une unité de temps.
+	 * @param t le temps courant.
+	 */
 	public void simulerEvolution(int t);
 }
