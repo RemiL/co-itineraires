b532255085ef56b03ec1ba00f0e04bed91e061b3
nivet1@hotmail.com <nivet1@hotmail.com@43486c22-1e13-4fa2-52d3-2fa976c108dc>
2010-12-15 17:30:12 +0000

    Création de la javadoc des classes du package MoyenTransport

--- trunk/src/moyenstransport/MoyenTransport.java
+++ trunk/src/moyenstransport/MoyenTransport.java
@@ -1,5 +1,9 @@
 package moyenstransport;
-
+/**
+ * interface MoyenTransport qui represente n'importe quel moyen de transport
+ * @author Marie
+ *
+ */
 public interface MoyenTransport
 {
 	
--- trunk/src/moyenstransport/PrendreBus.java
+++ trunk/src/moyenstransport/PrendreBus.java
@@ -1,14 +1,26 @@
 package moyenstransport;
-
+/**
+ * Class PrendreBus qui implémente MoyenTransport
+ * représente le moyen de transport du bus
+ * @author Marie
+ *
+ */
 public class PrendreBus implements MoyenTransport
 {
 	private static PrendreBus instance;
 	
+	/**
+	 * constructeur prive PrendreBus
+	 */
 	private PrendreBus()
 	{
 		
 	}
 	
+	/**
+	 * méthode getInstance 
+	 * @return instance une instance de PrendreBus
+	 */
     public static PrendreBus getInstance()
     {
         if (instance == null) 
@@ -19,6 +31,9 @@ public class PrendreBus implements MoyenTransport
         return instance;
     }
     
+    /**
+     * méthode toString qui indique que l'on est en mode bus
+     */
     public String toString()
     {
     	return "Prendre le bus";
