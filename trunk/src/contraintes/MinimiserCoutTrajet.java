9414531a47ad8fe1384e9e32b4958cce34eb1b89
remi.lacroix <remi.lacroix@43486c22-1e13-4fa2-52d3-2fa976c108dc>
2010-12-15 21:13:07 +0000

    - Ajout javadoc des classes de Contrainte

--- trunk/src/contraintes/MinimiserCoutTrajet.java
+++ trunk/src/contraintes/MinimiserCoutTrajet.java
@@ -4,13 +4,25 @@ import java.util.ArrayList;
 
 import trajet.Trajet;
 
+/**
+ * Classe implémentant une contrainte élémentaire visant à sélectionner uniquement
+ * les trajets ayant le coût global minimum.
+ */
 public class MinimiserCoutTrajet extends ContrainteElementaire
 {
+	/**
+	 * Construit une contrainte visant à minimiser le coût global des trajets.
+	 */
 	public MinimiserCoutTrajet()
 	{
 		
 	}
 	
+	/**
+	 * Permet d'évaluer la liste de trajets fournie au sens de la contrainte.
+	 * @param la liste des trajets candidats.
+	 * @return la liste des meilleurs trajets au sens de la contrainte.
+	 */
 	public ArrayList<Trajet> evaluerTrajets(ArrayList<Trajet> trajetsCandidats) 
 	{
 		ArrayList<Trajet> meilleursTrajets = new ArrayList<Trajet>();
@@ -18,10 +30,10 @@ public class MinimiserCoutTrajet extends ContrainteElementaire
 		
 		for (Trajet trajet : trajetsCandidats)
 		{
-			if (cout > trajet.getCout())
-			{
+			if (cout > trajet.getCout()) // On a un trajet moins couteux
+			{ 
 				cout = trajet.getCout();
-				meilleursTrajets.clear();
+				meilleursTrajets.clear(); // On vide l'ancienne liste des trajets sélectionnés
 				meilleursTrajets.add(trajet);
 			}
 			else if (cout == trajet.getCout())
@@ -33,6 +45,10 @@ public class MinimiserCoutTrajet extends ContrainteElementaire
 		return meilleursTrajets;
 	}
 	
+	/**
+	 * Retourne une représentation textuelle de la contrainte.
+	 * @return une chaine de caractères représentant la contrainte.
+	 */
 	public String toString()
 	{
 		return super.toString()+" : Minimiser le coût global du trajet";
