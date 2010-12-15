10c6a3341beaa2175263ba1d40d51cf30a183026
nivet1@hotmail.com <nivet1@hotmail.com@43486c22-1e13-4fa2-52d3-2fa976c108dc>
2010-12-15 17:24:14 +0000

    Création de la javadoc de la classe EtapeNonExistanteException.java Création de la javadoc de la classe TrajetNonCoherentException.java (à revoir)

--- src/trajet/EtapeNonExistanteException.java
+++ src/trajet/EtapeNonExistanteException.java
@@ -2,11 +2,21 @@ package trajet;
 
 import graphe.Etape;
 
+/**
+ * class EtapeNonExistanteException qui hérite de TrajetException
+ * exception qui attrape un trajet non existant
+ * @author Marie
+ *
+ */
 public class EtapeNonExistanteException extends TrajetException
 {
 	private static final long serialVersionUID = -2620710761828646860L;
 	private Etape etape;
 	
+	/**
+	 * constructeur EtapeNonExistanteException
+	 * @param etape : etape inexistante
+	 */
 	public EtapeNonExistanteException(Etape etape)
 	{
 		super();
