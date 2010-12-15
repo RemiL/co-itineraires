1a23c3cb9071b3eb5fcb844b32ef20a8d9b5e0ad
nivet1@hotmail.com <nivet1@hotmail.com@43486c22-1e13-4fa2-52d3-2fa976c108dc>
2010-12-15 17:18:01 +0000

    Création de la javadoc de la classe TrajetException.java

--- trunk/src/trajet/TrajetException.java
+++ trunk/src/trajet/TrajetException.java
@@ -1,9 +1,16 @@
 package trajet;
 
+/**
+ * class TrajetException 
+ * @author Marie
+ *
+ */
 public abstract class TrajetException extends Exception
 {
 	private static final long serialVersionUID = 702468081698212125L;
-	
+	/**
+	 * constructeur TrajetException
+	 */
 	public TrajetException()
 	{
 		super();
