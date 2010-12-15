5b34893f08b56e7253961e060958e971f5fbbeeb
nivet1@hotmail.com <nivet1@hotmail.com@43486c22-1e13-4fa2-52d3-2fa976c108dc>
2010-12-15 17:35:04 +0000

    Création de la javadoc des classes du package Ligne TO DO: javadoc méthode mettreAJourCoutEtDuree dans la class Ligne.java

--- src/ligne/Horaire.java
+++ src/ligne/Horaire.java
@@ -1,9 +1,19 @@
 package ligne;
 
+/**
+ * class Horaire représentant un horaire doté d'heures et de minutes
+ * @author Marie
+ *
+ */
 class Horaire
 {
 	int h, m;
 	
+	/**
+	 * constructeur Horaire
+	 * @param h les heures
+	 * @param m les minutes
+	 */
 	public Horaire(int h, int m)
 	{
 		this.h = h;
