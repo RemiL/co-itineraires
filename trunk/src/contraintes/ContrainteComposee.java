1521d6cc3ed9d6f3f9b25461b9a12cb464807786
remi.lacroix <remi.lacroix@43486c22-1e13-4fa2-52d3-2fa976c108dc>
2010-12-16 02:25:10 +0000

    - Correction : une contrainte composée ne peut contenir que des contraintes élémentaires.

--- trunk/src/contraintes/ContrainteComposee.java
+++ trunk/src/contraintes/ContrainteComposee.java
@@ -6,28 +6,28 @@ import trajet.Trajet;
 
 /**
  * Classe implémentant l'interface Contrainte et représentant une contrainte 
- * composée d'une ou plusieurs contraintes élémentaires ou non. Pour obtenir
- * les meilleurs trajets selon la contrainte composée, on applique d'abord la
+ * composée d'une ou plusieurs contraintes élémentaires. Pour obtenir les
+ * meilleurs trajets selon la contrainte composée, on applique d'abord la
  * première sous-contrainte PUIS la seconde et ainsi de suite.
  */
 public class ContrainteComposee implements Contrainte
 {
-	/** La liste des sous-contraites */
-	private ArrayList<Contrainte> contraintes;
+	/** La liste des sous-contraintes */
+	private ArrayList<ContrainteElementaire> contraintes;
 	
 	/**
 	 * Construit une contrainte composée vide
 	 */
 	public ContrainteComposee()
 	{
-		contraintes = new ArrayList<Contrainte>();
+		contraintes = new ArrayList<ContrainteElementaire>();
 	}
 	
 	/**
-	 * Permet d'ajouter une sous contraite à la contrainte composée.
+	 * Permet d'ajouter une contraite élémentaire à la contrainte composée.
 	 * @param contrainte la contraite à ajouter.
 	 */
-	public void ajouterContrainte(Contrainte contrainte)
+	public void ajouterContrainte(ContrainteElementaire contrainte)
 	{
 		contraintes.add(contrainte);
 	}
